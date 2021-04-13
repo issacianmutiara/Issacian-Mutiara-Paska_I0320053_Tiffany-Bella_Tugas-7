6800373d028902d47757ba85cf65d8fb21c66e0b
I Mutiara <issacianmutiara@gmail.com>
2021-04-13 21:00:20 +0700

    I0320053_Soal 2

---
+++ I0320053_Soal 2.py
@@ -0,0 +1,36 @@
+a = 'Percakapan Saat KBM'
+x = a.center(50)
+y = x.upper()
+print(y)
+
+#Fungsi Pow
+x = 5
+y = 2
+w = (pow(x,y))
+print('Guru : Hari ini yang masuk berapa siswa ya?')
+print('Ketua kelas : ', 'ada', w , 'anak bu')
+print('Baik, apakah ada yang ingin ditanyakan?')
+
+# Fungsi abs
+a = -2
+a2 = 'Bu guru hasil dari 3-1 berapa ya?'
+a3 = (abs(a))
+print('Murid : ', end = '')
+print(a2)
+print('Guru : ', end = '')
+print('Hasilnya adalah', a3, 'ya anak-anak')
+
+f = 'Beberapa saat kemudian'
+g = f.center(50, '-')
+print(g)
+
+#Fungsi Ceil
+print('Sekarang ibu akan memberi contoh soal pembulatan, anak anak mau angka berapa?')
+import math
+h = float(input('masukkan angka yang anda inginkan : '))
+i = (math.ceil(h))
+print('Oke pembulatan dari', h,'adalah', i, 'ya anak anak')
+
+
+
+
