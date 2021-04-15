b0fa8d4ce57bcaf96c9622d96b28abeff1e95c13
I Mutiara <issacianmutiara@gmail.com>
2021-04-15 23:39:09 +0700

    I0320053_Ex 25

---
+++ I0320053_Ex 25.py
@@ -0,0 +1,9 @@
+#metode items,keys,dan value
+a = {'susu': 10, 'dua':20, 'tiga': 30}
+print('a= ',a)
+item = a.items()
+keys = a.keys()
+value = a.values()
+print('items= ', item)
+print('keys= ',keys)
+print('value= ',value)
