dd4d26c880dc2d9c12178e7b985dbbfce6050b7c
I Mutiara <issacianmutiara@gmail.com>
2021-04-15 21:56:13 +0700

    I0320053_Ex 21

---
+++ I0320053_Ex 21.py
@@ -0,0 +1,11 @@
+#Fungsi choice
+
+import random
+a = [20, 30, 35, 40]
+print('a =', a)
+print('random 1')
+print('choice = ', random.choice(a))
+print('random 2')
+print('choice= ', random.choice(a))
+print('random 3')
+print('choice= ', random.choice(a))
