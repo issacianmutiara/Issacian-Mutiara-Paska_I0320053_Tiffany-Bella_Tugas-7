fc9acda4cfccc0217f955949afedb63c59e5d516
I Mutiara <issacianmutiara@gmail.com>
2021-04-15 20:00:50 +0700

    I0320053_Ex 04

---
+++ I0320053_Ex 04.py
@@ -0,0 +1,10 @@
+def panggil(func):
+    return func
+
+def issacian():
+    return 'ISSACIAN'
+def main():
+    s = panggil(issacian())
+    print(s)
+    if __name__ == '__main__':
+        main()
